efb1d8d8b45c50e2f4331ae36d23748c498d2c6a
BiCheng Sha <BiChengSha@users.noreply.github.com>
2020-05-28 13:21:38 -0400

    Update hello.c

--- pset1/hello/hello.c
+++ pset1/hello/hello.c
@@ -1,1 +1,9 @@
-//placeholder text
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    //printf("hello, world\n");
+    string username = get_string("What is your name?: ");
+    printf("hello, %s\n", username);
+}
